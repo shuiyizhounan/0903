7078b98ff5169e37a1f8980de6a7157653b84d52
shuiyi <815371927@qq.com>
2021-09-03 15:47:00 +0800

    新项目：学生类2

--- src/com/liu/entity/Student.java
+++ src/com/liu/entity/Student.java
@@ -3,13 +3,22 @@ package com.liu.entity;
 public class Student {
     private int sid;
     private String sname;
-
+    private int age;
     public Student() {
     }
 
-    public Student(int sid, String sname) {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student(int sid, String sname,int age) {
         this.sid = sid;
         this.sname = sname;
+        this.age = age;
     }
 
     public int getSid() {
